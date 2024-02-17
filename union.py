794312eb1aca809da60c8c26fafa32d57563a0ad
JorgeGarcia105 <jorge.garcia5887@ucaldas.edu.co>
2024-02-17 16:18:30 -0500

    Sistema operativo con python y c

---
+++ union.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+# Definir la ventana principal
+root = tk.Tk()
+root.title("Sistema Operativo")
+
+# Agregar widgets (botones, etiquetas, etc.) a la ventana
+
+# Definir funciones para manejar eventos
+
+# Iniciar el bucle principal de la interfaz de usuario
+root.mainloop()
