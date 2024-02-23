f5e9d9297ccf2f6812415b2160758675dd27420a
JorgeGarcia105 <jorge.garcia5887@ucaldas.edu.co>
2024-02-22 20:26:47 -0500

    Division de proyecto GarciaOS

--- vacio/c_cpp_code/sum.h
+++
@@ -1,9 +0,0 @@
-// sum.h
-#ifndef SUM_H
-#define SUM_H
-
-extern "C" {
-    int sum(int a, int b);
-}
-
-#endif
--- vacio/c_cpp_code/sum_wrapper.c
+++
@@ -1,7 +0,0 @@
-// sum_wrapper.c
-#include "sum.h"
-#include "sum_wrapper.h"
-
-int sum_c(int a, int b) {
-    return sum(a, b);
-}
